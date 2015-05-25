b1c06c226300dbddea163b1436d3dc1cda77b284
Leonardo Jofré <ljofre2146@gmail.com>
2015-05-25 03:37:58 -0300

    cambios menores

--- app/elements/speech-element/speech-element.js
+++ app/elements/speech-element/speech-element.js
@@ -6,26 +6,28 @@ Polymer({
             var speechMic = this.$.speechMic;
             var speechPhrase = this.$.speechPhrase;
             var check_icon = this.$.check_icon;
-            speechInput.style.display='none';
+            speechInput.style.display = 'none';
             //Separamos las palabras que son suseptibles de reconocer
             var words = speechPhrase.innerText.split('/');
             console.log("Palabras:")
             console.log(words);
 
+            //obtener la lista de homonimos
+            //words = sound_like(speechPhrase.innerText);
+            check_icon.style.color = "black";
             speechMic.addEventListener('speech-mic-result', function (e) {
                   speechInput.value = e.detail.transcript;
                   //Mostramos la palabra reconocida
                   console.log(speechInput.value.toLowerCase());
                   //Iteramos sobre el listado de palabras buscando la palabra reconocida
-                  for(var i in words){
+                  for (var i in words) {
                         if (speechInput.value.toLowerCase() == words[i]) {
                               speechMic.stop();
                               console.log("La palabra coincidio con: " + words[i]);
                               check_icon.style.color = "green";
-                              i = words.length; 
+                              i = words.length;
                         }
                         else {
-                              //console.log("no es igual");
                               check_icon.style.color = "red";
                         }
                   }
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,16 @@
-(function (document) {
-  'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+var natural = require("natural");
+var express = require("express");
+var serve = require("serve");
+var http = require("http");
+var fs = require("fs");
+var connect = require("connect");
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
-  });
+var app = connect();
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+function onRequest(request, response) {
+
+}
+
+http.createServer(onRequest).listen(8888);
+
