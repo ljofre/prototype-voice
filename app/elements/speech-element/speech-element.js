4d23371897e8e292c9e61bdc6911e1fbe1ecfeee
Camilo Ramirez <cramyz@gmail.com>
2015-06-07 13:50:21 -0300

    cambio js reconocimiento palabras

--- app/elements/speech-element/speech-element.js
+++ app/elements/speech-element/speech-element.js
@@ -1,40 +1,42 @@
 
 Polymer({
-      ready: function () {
-            var speechInput = this.$.speechInput;
-            var speechMic = this.$.speechMic;
-            var speechPhrase = this.$.speechPhrase;
-            var check_icon = this.$.check_icon;
-            speechInput.style.display = 'none';
-            //Separamos las palabras que son suseptibles de reconocer
-            var words = speechPhrase.innerText.split('/');
-            console.log("Palabras:");
-            console.log(words);
+    ready: function () {
+        var speechInput = this.$.speechInput;
+        var speechMic = this.$.speechMic;
+        var speechPhrase = this.$.speechPhrase;
+        var check_icon = this.$.check_icon;
+        speechInput.style.display = 'none';
+        //Separamos las palabras que son suseptibles de reconocer
+        var words = speechPhrase.innerText.split('/');
+        //console.log("Palabras:");
+        //console.log(words);
 
-            //obtener la lista de homonimos
-            //words = sound_like(speechPhrase.innerText);
-            check_icon.style.color = "black";
-            speechMic.addEventListener('speech-mic-result', function (e) {
-                  speechInput.value = e.detail.transcript;
-                  //Mostramos la palabra reconocida
-                  console.log(speechInput.value.toLowerCase());
-                  //Iteramos sobre el listado de palabras buscando la palabra reconocida
-                  for (var i in words) {
-                        if (speechInput.value.toLowerCase() == words[i]) {
-                              speechMic.stop();
-                              console.log("La palabra coincidio con: " + words[i]);
-                              check_icon.style.color = "green";
-                              i = words.length;
-                        }
-                        else {
-                              check_icon.style.color = "red";
-                        }
-                  }
-            });
-      },
-      speak: function(){
-            voice = this.$.voice;
-            voice.speak();
-      }
-      
+        //obtener la lista de homonimos
+        //words = sound_like(speechPhrase.innerText);
+        check_icon.style.color = "black";
+        speechMic.addEventListener('speech-mic-result', function (e) {
+            speechInput.value = e.detail.transcript;
+            //Mostramos la palabra reconocida
+            //console.log(speechInput.value.toLowerCase());
+            //Iteramos sobre el listado de palabras buscando la palabra reconocida
+            for (var i in words) {
+                if (speechInput.value.toLowerCase().trim() == words[i].toLowerCase().trim()) {
+                    speechMic.stop();
+                    console.log("La palabra coincidio con: " + words[i]);
+                    check_icon.style.color = "green";
+                    i = words.length;
+                }
+                else {
+                    console.log("no coincide " + words[i].toLowerCase() + " con " + speechInput.value.toLowerCase());
+
+                    check_icon.style.color = "red";
+                }
+            }
+        });
+    },
+    speak: function () {
+        voice = this.$.voice;
+        voice.speak();
+    }
+
 });
