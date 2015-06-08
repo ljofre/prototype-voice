bcd97aaf8a9bd698ed2153dd15da143e93146422
Leonardo Jofré <ljofre2146@gmail.com>
2015-06-08 11:44:31 -0300

    alternativas con reconocimiento de voz

--- app/elements/alternatives-speech-element/alternatives-speech-element.js
+++ app/elements/alternatives-speech-element/alternatives-speech-element.js
@@ -1,38 +1,21 @@
 Polymer("alternatives-speech-element", {
-    
-    //propiedades
-    state: false,
-    // metodos
+
     ready: function () {
+        var alternatives = JSON.parse(this.alternatives);
         this.alternatives = JSON.parse(this.alternatives);
-        this.choices = this.alternatives["alternatives"];
-        this.correct = this.alternatives["correct"];
-        this.$.voice.text = this.choices[this.correct];
-        
         var speechInput = this.$.speechInput;
         var speechMic = this.$.speechMic;
-       
         var check_icon = this.$.check_icon;
         speechInput.style.display = 'none';
         speechMic.style.display = 'none';
         //Separamos las palabras que son suseptibles de reconocer
         check_icon.style.color = "black";
-
-
-    },
-
-    domReady: function () {
         
-        var speechMic = this.$.speechMic;
-         var speechPhrase = this.$.speechPhrase;
-         var speechInput = this.$.speechInput;
-         var check_icon = this.$.check_icon;
-         
         speechMic.addEventListener('speech-mic-result', function (e) {
-            this.$.speechPhrase.textContent = this.choices[this.correct];
-            var words = speechPhrase.innerText.split('/');
             speechInput.value = e.detail.transcript;
-
+            var words = alternatives.choices[alternatives.correct];
+            words = words.split('/');
+            
             //Iteramos sobre el listado de palabras buscando la palabra reconocida
             for (var i in words) {
 
@@ -40,12 +23,10 @@ Polymer("alternatives-speech-element", {
                     speechMic.stop();
                     console.log("La palabra coincidio con: " + words[i]);
                     check_icon.style.color = "green";
-                    i = words.length;
                 }
 
                 else {
                     console.log("no coincide " + words[i].toLowerCase() + " con " + speechInput.value.toLowerCase());
-
                     check_icon.style.color = "red";
                 }
             }
