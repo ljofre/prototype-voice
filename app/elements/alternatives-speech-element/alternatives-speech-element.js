d523af7130b88594dead076a942f2538bf29ff11
Leonardo Jofré <ljofre2146@gmail.com>
2015-06-14 00:25:47 -0300

    iconos por defecto

--- app/elements/alternatives-speech-element/alternatives-speech-element.js
+++ app/elements/alternatives-speech-element/alternatives-speech-element.js
@@ -1,5 +1,4 @@
 (function () {
-    var procesando = false;
     Polymer("alternatives-speech-element", {
         //metodos
         ready: function () {
@@ -9,11 +8,6 @@
             this.$.spanish.style.display = "none";
         },
         resultEvent: function (e) {
-            console.log(procesando);
-            if (procesando) {
-                return;
-            }
-            procesando = true;
             var transcript = e.detail.transcript;
             var words = this.alternatives.choices[this.alternatives.correct].split('/');
             //Iter sobre el listado de palabras buscando la palabra reconocida
