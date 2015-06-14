40f33c130926dfcebeb9750128dffe4be9c48596
Leonardo Jofré <ljofre2146@gmail.com>
2015-06-13 21:48:31 -0300

    evento "success" para cuando las alternativas

--- app/elements/alternatives-speech-element/alternatives-speech-element.js
+++ app/elements/alternatives-speech-element/alternatives-speech-element.js
@@ -1,42 +1,37 @@
-Polymer("alternatives-speech-element", {
-
-    ready: function () {
-        this.alternatives = JSON.parse(this.alternatives);
-        var alternatives = this.alternatives;
-        var speechMic = this.$.speechMic;
-        var check_icon = this.$.check_icon;
-        speechMic.style.display = 'none';
-        check_icon.style.color = "black";
-
-        speechMic.addEventListener('speech-mic-result', function (e) {
+(function () {
+    Polymer("alternatives-speech-element", {
+        //metodos
+        ready: function () {
+            this.alternatives = JSON.parse(this.alternatives);
+            this.$.speechMic.style.display = 'none';
+            this.$.check_icon.style.color = "black";
+        },
+        resultEvent: function (e) {
             var transcript = e.detail.transcript;
-            var words = alternatives.choices[alternatives.correct];
-            words = words.split('/');
-            
+            var words = this.alternatives.choices[this.alternatives.correct].split('/');
             //Iter sobre el listado de palabras buscando la palabra reconocida
             for (var i in words) {
 
-                if (this.transcript.toLowerCase().trim().search(words[i].toLowerCase().trim()) != 0) {
-                    speechMic.stop();
+                if (transcript.toLowerCase().trim().search(words[i].toLowerCase().trim()) != 0) {
+                    this.$.speechMic.stop();
                     console.log("La palabra coincidio con: " + words[i]);
-                    check_icon.style.color = "green";
+                    this.$.check_icon.style.color = "green";
+                    this.fire("success");
                 }
 
                 else {
                     console.log("no coincide " + words[i].toLowerCase() + " con " + transcript);
-                    check_icon.style.color = "red";
+                    this.$.check_icon.style.color = "red";
                 }
             }
-        });
-    },
+        },
+        speak: function (e) {
+            var voiceElement = this.$.texts.querySelector('.' + e.srcElement.id);
+            voiceElement.speak();
+        },
+        listen: function () {
+            this.$.speechMic.toggleRecognition();
+        }
 
-    speak: function (e) {
-        var voiceElement = this.$.texts.querySelector('.' + e.srcElement.id);
-        voiceElement.speak();
-    },
-
-    listen: function () {
-        this.$.speechMic.toggleRecognition();
-    }
-
-});
+    });
+})();
--- app/elements/speech-element/speech-element.js
+++ app/elements/speech-element/speech-element.js
@@ -1,32 +1,24 @@
-
 (function () {
-    Polymer({
-        result: "",
+    Polymer("speech-element",{
         // metodos
         ready: function () {
-            var speechInput = this.$.speechInput;
-            var speechMic = this.$.speechMic;
-            var check_icon = this.$.check_icon;
-            speechInput.style.display = 'none';
-            speechMic.style.display = 'none';
-            //Separamos las palabras que son suseptibles de reconocer
-            check_icon.style.color = "black";
+            this.$.speechMic.style.display = 'none';
+            this.$.check_icon.style.color = "black";
         },
         resultEvent: function (e) {
-            this.$.speechInput.value = e.detail.transcript;
+            var transcript = e.detail.transcript;
             var words = this.$.speechPhrase.innerText.split('/');
-            //Iteramos sobre el listado de palabras buscando la palabra reconocida
+            //Iter sobre el listado de palabras buscando la palabra reconocida
             for (var i in words) {
 
                 if (this.$.speechInput.value.toLowerCase().trim() == words[i].toLowerCase().trim()) {
-
                     this.$.speechMic.stop();
                     console.log("La palabra coincidio con: " + words[i]);
                     this.$.check_icon.style.color = "green";
                     this.fire("success");
                 }
                 else {
-                    console.log("no coincide " + words[i].toLowerCase() + " con " + this.$.speechInput.value.toLowerCase());
+                    console.log("no coincide " + words[i].toLowerCase() + " con " + transcript.toLowerCase());
                     this.$.check_icon.style.color = "red";
                 }
             }
